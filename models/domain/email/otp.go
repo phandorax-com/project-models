0e0eb19894eeabf8157b1c04213c30f5268e0d75
Hernando Jiménez Z <hjimenezzambrano@gmail.com>
2023-10-05 22:35:49 -0500

    Update interfaces of queue and model data

---
+++ models/domain/email/otp.go
@@ -0,0 +1,8 @@
+package model
+
+type Otp struct {
+	Value string
+	Sub   string
+	Name  string
+	Email string
+}
---
+++ models/services/queue/message.go
@@ -0,0 +1,43 @@
+package queue
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Message[T any] struct {
+	Uuid       string            `json:"uuid"`
+	DataTime   time.Time         `json:"date_time"`
+	Aplication AplicationContext `json:"producer"`
+	Message    T                 `json:"message"`
+}
+
+type AplicationContext struct {
+	Name     string `json:"app_name"`
+	Tracking string `json:"tracking"`
+	UserID   string `json:"user_id"`
+}
+
+func SendMessage(aplication AplicationContext, value any) Message[any] {
+	return Message[any]{
+		Uuid:       uuid.NewString(),
+		DataTime:   time.Now().UTC(),
+		Aplication: aplication,
+		Message:    value,
+	}
+}
+
+func GetMessage(message string) Message[any] {
+
+	data := Message[any]{}
+
+	err := json.Unmarshal([]byte(message), &data)
+
+	if err != nil {
+		return data
+	}
+
+	return data
+}
